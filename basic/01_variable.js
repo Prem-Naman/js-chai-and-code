fae8ed2bffabc18a07addff7c2ecd0c20c19e831
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-09-18 13:54:33 +0000

    variable

---
+++ basic/01_variable.js
@@ -0,0 +1,26 @@
+const accountId=144553
+let accountEmail="namanprem492@gmail.com"
+var  accountPassword="12345"
+// accountId = 2
+accountPassword="34535"
+accountEmail="hello@gmail.com"
+accountcity="Patna"
+let accountstate;
+
+
+
+
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+
+
+
+*/
+
+console.log(accountId)
+console.log(accountstate,accountcity,accountEmail,accountPassword,accountId)
+
+
+
