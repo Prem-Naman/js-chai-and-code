3a7ffdfb017dc941a0b71fadcbbbcf9fe8053af5
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-09-18 15:15:05 +0000

    datatype

---
+++ basic/05_datatypes-summary.js
@@ -0,0 +1,38 @@
+//Primitive datatype
+
+
+// 7 types : String , Number ,boolean, null , undefined , symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3 
+
+const isloggedIn= false
+const outsideTemp= null 
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id==anotherid);
+
+
+const bigNumber = 34654353535435n
+
+// Reference (Non primitive )
+
+
+// Array , objects , function
+
+const heros =["shaktiman","naagraj","doga"];
+
+let obj ={
+
+    name:"Prem",
+    age:22,
+}
+const myFunction=function(){
+    console.log("hello wold ")
+}
+
+console.log(typeof bignumber);
+
