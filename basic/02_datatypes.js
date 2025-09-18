8958dd52f31a87fb945135e84dbb64722cf5bfa5
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-09-18 13:53:26 +0000

    datatypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"//new version of javascript in use 
+
+//alert(3+3)// we are using nodejs,not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+
+
+
+console.log("Prem")
+let name="Prem"
+let age = 18
+let isloggedIn=false
+let state= null 
+
+
+
+
+//number =>2 to poer 53
+//bigint
+//string =>""
+//boolean =>true/false
+// null=>standlone value
+// undefined 
+//symbol=>unique
+
+
+//object
+
+
+
+console.log(typeof"Prem");//undefined
+
+console.log(typeof null);//object
