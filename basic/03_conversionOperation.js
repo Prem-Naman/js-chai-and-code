c1ae5a6c4c1922cd1df7ae02360aeafdc8ce7e4e
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-09-18 14:38:30 +0000

    conversionOperation

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,57 @@
+// let score = "45abc"
+
+// console.log(typeof score );
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log( valueInNumber);
+
+
+//"45"=>45
+//"45abc"=>NaN
+//true=>1;false=>0
+
+
+
+// let isloggedIn=1
+
+
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+
+// let someNumber =String(someNumber )
+// console.log(someNumber);
+// console.log(typeofsomeNumber);
+
+//*******************Operation*********************** *//
+
+let value = 3
+let negvalue = -value 
+// console.log(negvalue);
+
+
+// let str1="hello"
+// let str2="prem"
+// let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2);
+// console.log (1+2+"2")
+
+console.log(+true )
+let num1 ,num2,num3
+
+
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
