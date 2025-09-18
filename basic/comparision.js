9aeee8b0d76da590e540a91c947a116426ef4a0c
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-09-18 14:46:52 +0000

    comparision

---
+++ basic/comparision.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+
+console.log("02">1);
+
+
+//two different datatype can not be compareable
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+
+
+
+
+
+//===
+
+
+console.log("2"===2)
+
+
