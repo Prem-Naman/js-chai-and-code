c87099eeb253dbbbb278c8e41ece8b5de70f3c5f
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-09-21 17:03:45 +0000

    string

---
+++ basic/06_string.js
@@ -0,0 +1,55 @@
+const name="Prem"
+const repocount = 50
+
+
+
+console.log(`hello my name ${name} and my repo coutn ${repocount}`);
+
+
+
+
+
+
+
+
+
+
+
+
+const gameName = new String('Rampoyon')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console. log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+
+console.log(gameName.charAt(2))
+const newString=gameName.substring(0,3)
+console.log(newString);
+
+
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+
+
+
+const newStringOne="   Prem   "
+
+
+console.log(newStringOne)
+console.log (newStringOne.trim())
+
+
+
+const url="https://prem.com/pre%20naman"
+
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('sundar'))
+
+
+
